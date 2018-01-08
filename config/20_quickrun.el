5dd4eb6291bf3fdb1bc01ec437e1d4565ae87870
uitspitss <uitspitss@gmail.com>
2018-01-09 03:22:56 +0900

    update

--- config/20_quickrun.el
+++ config/20_quickrun.el
@@ -6,7 +6,7 @@
 (global-set-key (kbd "C-c s") 'quickrun-shell)
 (global-set-key (kbd "C-c a") 'quickrun-with-arg)
 
-(setq quickrun-focus-p t)
+(setq quickrun-focus-p +1)
 (setq quickrun-timeout-seconds -1)
 
 (push '("*quickrun*" :position right :width 0.2 :noselect t :stick t) popwin:special-display-config)
